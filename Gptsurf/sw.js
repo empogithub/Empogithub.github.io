8c06a8484028c5a35045e5de7f8d8c3ab63679f8
empo <158759842+empogithub@users.noreply.github.com>
2025-08-10 17:08:06 +0200

    Create sw.js

---
+++ Gptsurf/sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('surf-game-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './script.js',
+        './manifest.json',
+        './icons/icon-192.png',
+        './icons/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
