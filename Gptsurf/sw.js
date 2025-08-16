c63305b146f50620da752966a18552c91b9665da
empo <158759842+empogithub@users.noreply.github.com>
2025-08-16 18:22:23 +0200

    Update sw.js

--- Gptsurf/sw.js
+++ Gptsurf/sw.js
@@ -7,7 +7,7 @@ self.addEventListener('install', event => {
         './style.css',
         './script.js',
         './manifest.json',
-        './icons/icon-192.png',
+        './',
         './icons/icon-512.png'
       ]);
     })
